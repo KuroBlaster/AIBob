9a6e7025ae3dd737bbfc8ee951e4ab882d33e837
KuroBlaster <kuromishra@gmail.com>
2015-07-05 22:10:29 -0700

    A basic Save/load function inputted, but having weird loops and losing track of what to save.

--- AIBob.java
+++ AIBob.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 		public static ArrayList<User> userList = new ArrayList<User>();
 
 		public static void main (String args[]){
+			
+			
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		
+			
 		Scanner scanner = new Scanner(System.in);
 			
 		System.out.println("Hello! My name is Bob, are you an existing user or you want to make a new accunt? ");	
@@ -17,77 +23,98 @@ import java.util.ArrayList;
 		
 		if(UserQuantifier.contains(check[0])){
 			
-		System.out.println("Can you tell me name your please?");
+		System.out.println("Can you tell me your name please?");
 		}
-		
-		String Givenname = scanner.nextLine();
-		
-		if(userList.contains(Givenname)){
-			// update the values in the user class
+		else{
+			NewUserPref(scanner);
 			
 		}
-		
-		System.out.println("Hello! My name is Bob, what is your name? ");
-		
+		String Givenname = scanner.nextLine();
 		
 		
-		//Create new user, assign name and user number
-		
-		User newUser = new User(scanner.nextLine());	//Takes the user input and saves into a User-name.
-		
-		userList.add(newUser); // Creates a new user in the User class.
-		
-		System.out.println("Hi "+newUser.getUserName()+"! Can you tell me how old are you?");
-		int Userage = scanner.nextInt();
-		newUser.setAge(Userage);
-		
-		System.out.println("If you are lactose intolerant, please write y/n ");
-		String input = scanner.next();		
-		newUser.LactoseFinder(input);
-		
-		System.out.println("If you want your food glutten free, please write y/n ");
-		String otherinput = scanner.next();		
-		newUser.GluttenFinder(otherinput);
-		
-		System.out.println("If you are vegetarian or non-vegetraian, please write y/n ");
-		String dainput = scanner.next();		
-		newUser.VegorNonveg(dainput);
-		
-		System.out.println("If you are vegan, please write y/n ");
-		String jainput = scanner.next();		
-		newUser.Vegan(jainput);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		System.out.println("Hi "+newUser.getUserName()+". What can I do for you today? \n");
-		
-		
-		
-		
-		// User UserName = new User();
-		
-		
-		String Foodwish = scanner.nextLine();
-		
-		FoodLibrary.FoodSorter(Foodwish); // Ask the user what sort of food he/she wants.
-		
-		System.out.println("What sort of Food do you want?");
-		String Demand = scanner.nextLine();
-		
-		FoodLibrary.FoodBringer(Demand); // Calls the FoodLibrary to ask what type of food the user wants.
-		
-		
-	*/
+		for(int i=0;i<=userList.size();i++){
+			if(userList.get(i).getUserName().equals(Givenname)){
+				userList.get(i).Load();
+				Questionasker(Givenname, scanner);
+			}
+			else{
+				NewUserPref(scanner);
+			}
 		}
 	
-	}
+		
+		
+		
+		
+		
+		}
 	
+	
+
+		public static void Questionasker(String Givenname, Scanner scanner){
+	
+	
+			System.out.println("Hi "+Givenname+". What can I do for you today? \n");
 		
-		
+			// User UserName = new User();
+	
+			String GivenFoodwish = scanner.nextLine();
+			String[] GFoodwish = {"Breakfast","Lunch","Dinner"};
+	
+			for(int i =0;i<GFoodwish.length;i++){
+				
+				if(GivenFoodwish.contains(GFoodwish[i])){
+					FoodLibrary.FoodSorter(GFoodwish[i]); // Ask the user what sort of food he/she wants.
+				}
+			}
+			
+			System.out.println("What sort of Food do you want?");
+	
+			String foodsort = scanner.nextLine();
+			String[] Gfoodsort = {"Mild","Spicy","Sweet"};
+	
+			for(int i =0;i<Gfoodsort.length;i++){
+				
+				if(foodsort.contains(GFoodwish[i])){
+					FoodLibrary.FoodBringer(Gfoodsort[i]); // Calls the FoodLibrary to ask what type of food the user wants.
+				}
+			}
+		}
+
+		public static void NewUserPref(Scanner scanner){
+	
+			System.out.println("Hello! My name is Bob, what is your name? ");
+	
+			String Gname = scanner.nextLine();
+	
+			//Create new user, assign name and user number
+	
+			User newUser = new User(Gname);	//Takes the user input and saves into a User-name.
+	
+			userList.add(newUser); // Creates a new user in the User class.
+	
+			System.out.println("Hi "+newUser.getUserName()+"! Can you tell me how old are you?");
+			int Userage = scanner.nextInt();
+			newUser.setAge(Userage);
+	
+			System.out.println("If you are lactose intolerant, please write y/n ");
+			String input = scanner.next();		
+			newUser.LactoseFinder(input);
+			
+			System.out.println("If you want your food glutten free, please write y/n ");
+			String otherinput = scanner.next();		
+			newUser.GluttenFinder(otherinput);
+	
+			System.out.println("If you are vegetarian or non-vegetraian, please write y/n ");
+			String dainput = scanner.next();		
+			newUser.VegorNonveg(dainput);
+	
+			System.out.println("If you are vegan, please write y/n ");
+			String jainput = scanner.next();		
+			newUser.Vegan(jainput);
+	
+			newUser.Save();
+		}
+	
+	
+}
--- User.java
+++ User.java
@@ -1,8 +1,12 @@
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class User {
 	
 	
-
 	private String userName;
 	private int age;
 	private boolean lactoseIntolerant;
@@ -11,17 +15,11 @@ public class User {
 	private boolean vegan;
 	
 	
-		
-	
-	
-	
 	//Default constructor for class User
 	public User(String userName)
 	{	
 		this.userName = userName;
-	
-	
-	}
+		}
 	
 	public String getUserName()
 	{
@@ -41,15 +39,12 @@ public class User {
 	{
 		return age;
 	}
-	
-	
-	
+		
 	 public void LactoseFinder(String value){						// Method to compute if Lactose-intolerant or not.
 		String yes = "y";  											// Could be an array of values
 		if(yes.equals(value)){										// /If array of values can use enchanced for loop
 			Boolean truthvalue = true;
-			
-		}
+			}
 		Boolean truthvalue = false;
 		lactoseIntolerant=truthvalue;
 		}
@@ -61,7 +56,6 @@ public class User {
 	
 	public void GluttenFinder(String value){						// Method to compute if Glutten-free or not.
 			String yes = "y";
-		
 		if(yes.equals(value)){
 			Boolean truthvalue = true;
 			
@@ -71,10 +65,8 @@ public class User {
 		}
 		
 	public boolean getGlutten(){
-	
-		return glutenFree;
-	
-	}
+			return glutenFree;
+		}
 		
 	public void VegorNonveg(String value){			// Method to compute if vegetarian or non-vegetarian
 		String yes = "y";							// Could be an array of values
@@ -105,8 +97,73 @@ public class User {
 		
 	public boolean getVegan(){
 		return vegan;
-	
 	}
+
+public void Save(){
+	try{  // Catch errors in I/O if necessary.
+		// Open a file to write to, named SavedObj.sav.
+		FileOutputStream saveFile=new FileOutputStream("SaveObj.sav");
+
+		// Create an ObjectOutputStream to put objects into save file.
+		ObjectOutputStream save = new ObjectOutputStream(saveFile);
+
+		// Now we do the save.
+		save.writeObject(userName);
+		save.writeObject(age);
+		save.writeObject(lactoseIntolerant);
+		save.writeObject(glutenFree);
+		save.writeObject(vegetarian);
+		save.writeObject(vegan);
+
+
+		// Close the file.
+		save.close(); // This also closes saveFile.
+		}
+		catch(Exception exc){
+		exc.printStackTrace(); // If there was an error, print the info.
+		}
+
+
+
 
 
 }
+public void Load(){
+	try{
+		// Open file to read from, named SavedObj.sav.
+		FileInputStream saveFile = new FileInputStream("SaveObj.sav");
+
+		// Create an ObjectInputStream to get objects from save file.
+		ObjectInputStream save = new ObjectInputStream(saveFile);
+
+		// Now we do the restore.
+		// readObject() returns a generic Object, we cast those back
+		// into their original class type.
+		// For primitive types, use the corresponding reference class.
+		userName = (String) save.readObject();
+		age = (Integer) save.readObject();
+		lactoseIntolerant = (Boolean) save.readObject();
+		glutenFree = (Boolean) save.readObject();
+		vegetarian = (Boolean) save.readObject();
+		vegan = (Boolean) save.readObject();
+		
+		// Close the file.
+		save.close(); // This also closes saveFile.
+		}
+		catch(Exception exc){
+		exc.printStackTrace(); // If there was an error, print the info.
+		}
+		
+}
+
+
+	
+}
+
+
+
+
+
+
+
+
